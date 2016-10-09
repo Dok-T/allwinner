58bcc9c43bfdece8b787b2493e622ddbf4c5e798
Doctor_Titi <DoctorTiti@users.noreply.github.com>
2016-10-09 09:52:14 +0200

    Tools
    Use "androtool" command in terminal to install some programs to compile Android.

---
+++ androtool.sh
@@ -0,0 +1,4 @@
+function androtool
+{
+sudo apt-get install git-core gnupg flex bison python rar gperf original-awk gawk p7zip-full libsdl1.2-dev libesd0-dev libwxgtk2.6-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev pngcrush schedtool libc6-dev x11proto-core-dev libx11-dev libg11-mesa-dev mingw32 tofrodos python-markdown libxml2-utils g++-multilib lib32z1-dev ia32-libs lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib g++-multilib xsltproc
+}
